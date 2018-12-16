306d45e41871e00dca67c44ced3e83913075f460
chen <chen@163.com>
2018-12-16 19:17:09 +0800

    修改代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,12 @@
-import org.junit.Test;
+
 
 public class Main {
 
-    @Test
     public void testHelloWrold() {
+        System.out.println("HelloWorld!");
+        System.out.println("w!");
+        System.out.println("y!");
+        System.out.println("n!");
         System.out.println("HelloWorld!");
     }
 }
